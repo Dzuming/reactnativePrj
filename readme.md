3804b9fd3fec29716651ee9336b4fb5876d7651d
stnc <stnc@stncs-iMac.local>
2019-07-05 14:43:16 +0300

    feat: new ext

---
+++ readme.md
@@ -0,0 +1,5 @@
+look 
+https://github.com/nathvarun/WTF-Is-Redux-React-Native-Tutorials/tree/4-TodoApp-Connect-Dispatch
+
+
+https://blog.usejournal.com/react-native-and-redux-part-5-3185f8f0609b
---
+++ superSimple/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import { connect } from 'react-redux';
+
+import {  testAction} from './redux';
+import { store } from './redux';
+import { Provider } from 'react-redux';
+
+
+class AppInner extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Button title="Press Me" onPress={this.props.testAction}>Test</Button>
+        <Text> {this.props.content}</Text>
+      </View>
+    );
+
+
+  }
+}
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <AppContainer />
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+// AppContainer.js
+const mapStateToProps = (state) => ({
+  content: state.dvas0004.content || "Please Wait...",
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    testAction: () => {
+      dispatch(testAction())
+    }
+  }
+};
+
+const AppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AppInner);

3d52bf246c3d113712d220fbc31acf4f9cfbbd53
stnc <stnc@stncs-iMac.local>
2019-07-13 01:58:03 +0300

    feat: path change

--- styles.js
+++ styles.js
@@ -5,6 +5,9 @@ const { width, height } = Dimensions.get('window');
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
+
+
+    marginTop:10,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#ffffff',
